f6cbcd155178c56ab5479a151eb132289c003157
Abhinav Pobbati <apobbati@codeblock.io>
2016-04-10 21:33:33 -0400

    Initial commit

---
+++ src/main/java/com/shipfast/Application.java
@@ -0,0 +1,71 @@
+package com.shipfast;
+
+import com.mongodb.MongoClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan("com.shipfast")
+public class Application extends SpringBootServletInitializer {
+	@Autowired
+	private Environment environment;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+	@Configuration
+	static class LocalConfig {
+		public @Bean MongoDbFactory mongoDbFactory() throws Exception {
+			return new SimpleMongoDbFactory(new MongoClient(), "inventory");
+		}
+
+		public @Bean MongoTemplate mongoTemplate() throws Exception {
+			return new MongoTemplate(mongoDbFactory());
+		}
+	}
+
+	@Configuration
+	@Profile("docker")
+	static class DockerConfig {
+		public @Bean MongoDbFactory mongoDbFactory() throws Exception {
+			return new SimpleMongoDbFactory(new MongoClient("inventory-db"), "inventory");
+		}
+
+		public @Bean MongoTemplate mongoTemplate() throws Exception {
+			return new MongoTemplate(mongoDbFactory());
+		}
+	}
+
+	@Bean
+	public Services services() {
+		return new Services(environment);
+	}
+
+	@Bean
+	public DispatcherServlet dispatcherServlet() {
+		return new DispatcherServlet();
+	}
+
+	@Bean
+	public InventoryService inventoryService() {
+		return new InventoryService();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+}
